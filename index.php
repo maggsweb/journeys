22d8e4aba253ae5256d4d15138eea6385d79759c
Chris Maggs <git@maggsweb.co.uk>
2022-08-18 10:30:16 +0100

    Add google chart

--- index.php
+++ index.php
@@ -26,12 +26,74 @@ if ($_SESSION['message'] ?? false) {
 <?php
 
 // Get records
-$records = $db->query('SELECT * FROM `journeys`')->fetchAll();
+$records = $db->query('SELECT * FROM `journeys` ORDER BY start_date')->fetchAll();
 //dump($records);
 
 ?>
 
-<br>:wq
+<br>
+
+
+
+<?php
+    $data = [];
+    $data[] = ['Date', 'Average Speed', 'Efficiency'];
+    foreach($records as $record) {
+        $data[] = [$record->start_date, (float)$record->speed, (float)$record->efficiency];
+    }
+    if (count($data)) {
+        $data = json_encode($data);
+    }
+//    dump($data);
+?>
+
+<div id="current_usage"></div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script>
+    google.load("visualization", "1", {packages:["CoreChart"], callback: drawCharts});
+    function drawCharts() {
+        drawChart_1();
+    }
+
+    function drawChart_1() {
+        let data = google.visualization.arrayToDataTable(<?=$data?>);
+        let options = {
+            //title:'Average Speed & Effiency',
+            legend: {
+                // position: "none"
+            },
+            height: 600,
+            colors: ['#326295', 'red'],
+            chartArea: {
+                top:30,
+                bottom: 80,
+            },
+            vAxis: {
+                //title: 'qwertgyhjuk'
+            },
+            hAxis: {
+                slantedText: true,
+                textStyle : {
+                    fontSize: 12
+                }
+            }
+        };
+        // var chart = new google.visualization.BarChart(document.getElementById('current_usage'));
+        let chart = new google.visualization.ColumnChart(document.getElementById('current_usage'));
+            chart.draw(data, options);
+    }
+
+    // $(document).ready(function(){
+
+        // $("a[href='#tab_1']").on('shown.bs.tab', function (e) {
+        //     drawChart_1();
+        // });
+    // });
+
+</script>
+
 
 <p>
     <a href="?view=data">Data</a> |
